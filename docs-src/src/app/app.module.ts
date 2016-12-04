94c3a5f45da6c41b44dc9b783b8bc81316315fa2
Zachary Fantauzzi <zackfantauzzi@gmail.com>
2016-12-04 14:20:34 -0500

    WIP: building docs stie

--- docs-src/src/app/app.module.ts
+++ docs-src/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
