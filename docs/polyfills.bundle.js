9937b43fc56955820697f1cb96e1f9300bf53fc5
Jake Stewart <jake.stewart@nbcuni.com>
2017-05-17 10:57:13 -0400

    Add export icons. (#39)
    * Add export icons.
    
    * Use auto generated icons html

--- docs/polyfills.bundle.js
+++ docs/polyfills.bundle.js
@@ -2809,7 +2809,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 // NgClass on SVG elements
 // Needed for: IE10, IE11
 // import 'classlist.js';
-//# sourceMappingURL=/Users/a206525964/projects/suprematism/suprematism-style-guide/docs-src/src/polyfills.js.map
+//# sourceMappingURL=/Users/jacobstewart/src/suprematism-style-guide/docs-src/src/polyfills.js.map
 
 /***/ }),
 /* 481 */,
